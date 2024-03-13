663519502349c5f990d2c5dba5a8dee98f236161
tle297 <thienhoang.le@dxc.com>
2024-03-13 15:35:17 +0700

    styling pizza card . Adding pagination + likelist function

--- src/components/Menu/PizzaCard/PizzaCard.jsx
+++ src/components/Menu/PizzaCard/PizzaCard.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./PizzaCard.css"
+import { FcLike } from "react-icons/fc";
+import { AiOutlineLike } from "react-icons/ai";
+//import { FcLike } from "react-icons/fc";
 export default function PizzaCard(props) {
   const {info} =  props;
+  const [like,setLike]=useState(false);
+  function handleLike(e){
+    e.preventDefault();
+    setLike((prev)=>!prev);
+  }
   return (
       <>
       <li key={info?.id} className={`pizza ${info?.soldOut ? "sold-out" : ""}`}>
-        <img src={info.photoName} alt={info.name}></img>
-        <div>
+        <div className='pizza__left'>
+          <img src={info.photoName} alt={info.name}></img>
+          {
+            like ? 
+            <FcLike className='likeIcon' onClick={(e)=>handleLike(e)} /> : 
+            <AiOutlineLike className='likeIcon' onClick={(e)=>handleLike(e)}></AiOutlineLike>
+          }
+          
+        </div>
+        <div className='pizza__right'>
           <h3>{info?.name}</h3>
           <p>{info?.ingredients}</p>
           <span>{info?.price}</span>
